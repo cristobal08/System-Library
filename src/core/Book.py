8845272f143461a927aac7225b4870dec1ad768e
cristobal08 <jescri.loup@gmail.com>
2025-08-28 22:51:58 -0600

    feat: add book class

--- src/core/Book.py
+++ src/core/Book.py
@@ -1,13 +1,26 @@
+from utils.DbLibrary import BookInfo
 
 class Book:
-    def __init__(self,isbn,title,autor,date_publication,genre,available_copies,state):
+    def __init__(self,isbn= None, title= None, autor= None, available_copies= None, state= None):
         self.isbn = isbn
         self.title = title
         self.autor = autor
-        self.date_publication = date_publication
-        self.genre = genre
         self.available_copies = available_copies
         self.state = state
+    
+    def add_book(self):
+        book_info = {}
+
+        book_info["isbn"] = input("ISBN Book: ")
+        book_info["title"] = input("Book Title: ")
+        book_info["autor"] = input("Autor: ") 
+        book_info["available_copies"] = int(input("Number of copies avaibles: "))
+        book_info["state"] =  input("Book state on rent/no aviable/ avaible: ")
+
+        return book_info
+    
+    def save_book(self,book_data):
+        BookInfo.create(**book_data)
 
     def show_book_info(self):
         print("======= Book Information ======")
@@ -25,10 +38,3 @@ class Book:
         print("======= Book ======")
         book_availability = input ("Type book's ISBN or Title (availability):_ ")
         print(f"availability: {self.available_copies}")
-
-
-
-myclas = Book(564,"title","michel","02/1/2025","horror",6,"avaiable")
-
-myclas.show_book_info()
-myclas.update_book_state()
--- src/core/Library.py
+++ src/core/Library.py
@@ -1,9 +1,11 @@
 from core.User import User
+from core.Book import Book
 
 class Library:
 
     def __init__(self):
         self.user = User()
+        self.book = Book()
 
     def menu(self):
         
@@ -12,6 +14,11 @@ class Library:
             print("1: add user: ")
             print("2: remove user: ")
             print("3: uptade user: ")
+
+            print("\n--- Books options --- \n")
+            print("4: add book: ")
+            print("5: remove book: ")
+            print("6: uptade book: ")
 
             option_registry = int(input("Chose option for user: "))
 
@@ -26,5 +33,11 @@ class Library:
                 case 3:
                     print("\n Update information User \n")
                     self.user.update_user()
+                case 4:
+                    print("Please , type the book information")
+                    book_data = self.book.add_book()
+                    self.book.save_book(book_data)
+
+        
         except ValueError:
             print(" choose an option !!!")
