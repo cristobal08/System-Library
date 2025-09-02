73ec691a76fd7cfc68d8a115e6cbd15c32dd8648
cristobal08 <jescri.loup@gmail.com>
2025-09-01 22:43:43 -0600

    feat: update menu book

--- src/core/Library.py
+++ src/core/Library.py
@@ -8,36 +8,48 @@ class Library:
         self.book = Book()
 
     def menu(self):
-        
-        try:
-            print("\n--- Library System Menu --- \n")
-            print("1: add user: ")
-            print("2: remove user: ")
-            print("3: uptade user: ")
+        while True:
+            try:
+                print("\n--- Library System Menu --- \n")
+                print("1: add user: ")
+                print("2: remove user: ")
+                print("3: update user: ")
+                print("\n--- Books options --- \n")
+                print("4: add book: ")
+                print("5: remove book: ")
+                print("6: update book: ")
+                print("\n Circulation (Library loans)")
+                print("7: Book lending: ")
+                print("8: EXIT")
 
-            print("\n--- Books options --- \n")
-            print("4: add book: ")
-            print("5: remove book: ")
-            print("6: uptade book: ")
+                option_registry = int(input("Choose option: "))
 
-            option_registry = int(input("Chose option for user: "))
+                match option_registry:
+                    case 1:
+                        print("\n Fill the information...")
+                        data = self.user.create_user()
+                        self.user.save_info_user(data)
+                    case 2:
+                        print(" \n Removing user from system \n")
+                        self.user.remove_user()
+                    case 3:
+                        print("\n Update information User \n")
+                        self.user.update_user()
+                    case 4:
+                        print("Please, type the book information")
+                        book_data = self.book.add_book()
+                        self.book.save_book(book_data)
+                    case 5:
+                        book_title = input("Book title to be removed: ")
+                        self.book.remove_book(book_title)
 
-            match option_registry:
-                case 1:
-                    print("\n Fill the information , write N/A if perfil dont apply \n")
-                    data = self.user.create_user()
-                    self.user.save_info_user(data)
-                case 2:
-                    print(" \n Removing user from system \n")
-                    self.user.remove_user()
-                case 3:
-                    print("\n Update information User \n")
-                    self.user.update_user()
-                case 4:
-                    print("Please , type the book information")
-                    book_data = self.book.add_book()
-                    self.book.save_book(book_data)
-
-        
-        except ValueError:
-            print(" choose an option !!!")
+                    case 8:
+                        print("Goodbye!")
+                        break 
+                    case _:
+                        print("Invalid option!")
+                
+                
+                input("\nPress ENTER to continue...")
+            except ValueError:
+                print(" choose an option !!!")
