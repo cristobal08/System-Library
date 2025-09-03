34b7e809850f64ce9c96235e06ee16e843c20105
cristobal08 <jescri.loup@gmail.com>
2025-09-02 22:30:52 -0600

    feat: updated tables

--- src/utils/DbLibrary.py
+++ src/utils/DbLibrary.py
@@ -4,7 +4,7 @@ from lightdb.models import Model
 # Initialize the database
 db_library = LightDB("src/data/library.json")
 
-class UserStudent(Model, table="UserInfo", database=db_library):
+class UserStudent(Model, table="StudentInfo", database=db_library):
     user_type: str
     user_name: str
     user_last_name: str
@@ -13,7 +13,7 @@ class UserStudent(Model, table="UserInfo", database=db_library):
     major: str  
     semester: int
 
-class UserTeacher(Model, table="UserInfo", database=db_library):
+class UserTeacher(Model, table="TeacherInfo", database=db_library):
     user_type: str
     user_name: str
     user_last_name: str
